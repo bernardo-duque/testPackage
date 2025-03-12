b16d5689a6544cd142a08758b229a7df2dacbfc9
bernardo_duque <bernardo_renno_duque@brown.edu>
2025-03-12 11:38:28 -0400

    fixed mytheme

--- R/my_theme.R
+++ R/my_theme.R
@@ -3,7 +3,7 @@
 #' @return customized ggplot theme
 #' @export
 my_theme <- function() {
-  ggplot + theme(panel.grid.major.x = element_blank(),
+  theme(panel.grid.major.x = element_blank(),
         panel.grid.minor.x = element_blank(),
         plot.title = element_text(hjust = 0.5,face = "bold"),
         axis.text = element_text(face = "bold",size = 14),
